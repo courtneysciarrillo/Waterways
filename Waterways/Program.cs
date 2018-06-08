8367f0dcf5001031a483dc1b5f2cdcfc68521f7d
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-06-08 14:14:54 -0400

    Initial Commit

---
+++ Waterways/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            River ohio = new River("Ohio", 35, 3);
+            Console.WriteLine("Welcome to Waterways Management Console");
+            Console.WriteLine("Please type Yes to manage the {0} river?", ohio.Name);
+            string userResponse = Console.ReadLine().ToLower();
+            while (userResponse.Equals("yes"))
+            {
+                Console.WriteLine("Please select from the following to manage the {0} river", ohio.Name);
+
+                Console.WriteLine("Select 1 to get the status of the {0} river", ohio.Name);
+                Console.WriteLine("Select 2 to change the depth of the {0} river", ohio.Name);
+                Console.WriteLine("Select 3 to clean the {0} river", ohio.Name);
+                int userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        ohio.GetStatus();
+                        if (ohio.DepthAlert)
+                        {
+                            Console.WriteLine("The {0} river is {1} deep and needs to be drained.", ohio.Name, ohio.Depth);
+                            Console.WriteLine("Please enter a new depth for the {0} river", ohio.Name);
+                            int newDepth = int.Parse(Console.ReadLine());
+                            ohio.SetDepth(newDepth);
+
+                        }
+                        if (ohio.PolAlert)
+                        {
+                            Console.WriteLine("The {0} river is above safe pollution standards and need to be cleaned.", ohio.Name);
+                            Console.WriteLine("Please enter a new pollutution level for the {0} river", ohio.Name);
+                            //int pollLevel = int.Parse(Console.ReadLine());
+                            ohio.SetPol(int.Parse(Console.ReadLine()));
+
+                        }
+                        Console.WriteLine("The {0} river is {1} deep and has a pollution level of {2} ppm", ohio.Name, ohio.Depth, ohio.Pollution);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter a new depth.");
+                        int depth = int.Parse(Console.ReadLine());
+                        ohio.SetDepth(depth);
+                        Console.WriteLine("The new depth of the {0} river is {1}", ohio.Name, ohio.Depth);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter a new pollution level.");
+                        int pollution = int.Parse(Console.ReadLine());
+                        ohio.SetPol(pollution);
+                        Console.WriteLine("The new pollution level of the {0} river is {1}", ohio.Name, ohio.Pollution);
+                        break;
+                    default:
+                        break;
+
+                }
+                Console.WriteLine("Please type Yes to manage the {0} river?", ohio.Name);
+                userResponse = Console.ReadLine().ToLower();
+            }
+
+
+
+        }
+    }
+}
---
+++ Waterways/River.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterways
+{
+    class River
+    {
+        private int depth;
+        private int pollution;
+        private string name;
+        private bool depthAlert;
+        private bool polAlert;
+
+        public bool DepthAlert
+        {
+            get { return this.depthAlert; }
+            set { this.depthAlert = value; }
+        }
+
+        public bool PolAlert
+        {
+            get { return this.polAlert; }
+            set { this.polAlert = value; }
+        }
+        public int Depth
+        {
+            get { return this.depth; }
+            set { this.depth = value; }
+        }
+        public int Pollution
+        {
+            get { return this.pollution; }
+            set { this.pollution = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public River()
+        {
+            //default constructor
+        }
+
+        public River(string name, int depth, int pollution)
+        {
+            this.Name = name;
+            this.Depth = depth;
+            this.Pollution = pollution;
+        }
+
+        public void GetStatus()
+        {
+            Depth++;
+            if (Depth > 25)
+            {
+                DepAlert(true);
+            }
+            else
+            {
+                DepAlert(false);
+            }
+            Pollution++;
+            if (Pollution > 5)
+            {
+                PlAlert(true);
+            }
+            else
+            {
+                PlAlert(false);
+            }
+        }
+
+        public bool DepAlert(bool status)
+        {
+            DepthAlert = status;
+            return DepthAlert;
+        }
+        public bool PlAlert(bool status)
+        {
+            PolAlert = status;
+            return PolAlert;
+        }
+
+        public int SetDepth(int num)
+        {
+            this.Depth = num;
+            return Depth;
+        }
+        public int SetPol(int num)
+        {
+            this.Pollution = num;
+            return Pollution;
+        }
+
+        public string SetName(string name)
+        {
+            this.Name = name;
+            return Name;
+        }
+
+    }
+}
